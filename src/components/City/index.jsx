31c78febba025917f147cd149a34a0641044ef42
vxshugo <vxshugo@gmail.com>
2023-05-02 19:57:47 +0600

    fix

---
+++ src/components/City/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const City = () => {
+    return (
+        <div>
+            <iframe
+                src="https://yandex.ru/map-widget/v1/?um=constructor%3A281d74ca4fb419a8d65cecdc23fd7c27591051ae0e9f3cce293a7612010e8b86&amp;source=constructor"
+                width="100%"
+                height="400"
+                frameborder="0"
+            ></iframe>
+        </div>
+    )
+}
+
+export default City
--- src/pages/main/Main.jsx
+++ src/pages/main/Main.jsx
@@ -3,12 +3,14 @@ import NavbarMain from '../../components/navbar'
 import MainBlock from "../../components/main"
 import UniversityInfo from '../../components/UniversityInfo'
 import Footer from '../../components/footer'
+import City from '../../components/City'
 function Main() {
   return (
     <div>
         <NavbarMain/>
         <UniversityInfo/>
         <MainBlock/>
+        <City/>
         <Footer/>
     </div>
   )
